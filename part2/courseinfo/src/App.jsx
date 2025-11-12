c0090cdd5ffb64bd5bdf35b8872ead2d6c4a775a
Pedro Valdebenito <pedrovaldebto@gmail.com>
2025-11-11 22:07:11 -0300

    complete exercises 2.1-2.3

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+const Total = ({ parts }) => (
+  <p>
+    Total of exercises{" "}
+    {parts.reduce((acumulator, part) => part.exercises + acumulator, 0)}
+  </p>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
