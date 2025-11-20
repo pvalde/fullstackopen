b065ea0949181e250773634d11f736a87c8e3aaf
Pedro Valdebenito <pedrovaldebto@gmail.com>
2025-11-20 09:54:55 -0300

    complete exercises 3.2-3.6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -25,8 +26,66 @@ const persons = [
   },
 ];
 
+const generateId = () => {
+  const maxId =
+    persons.length > 0
+      ? Math.max(...persons.map((person) => Number(person.id)))
+      : 0;
+  return String(maxId + 1);
+};
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const responseHtml =
+    `<p>Phonebook has info for ${persons.length} people.</p>` +
+    `<p>${date.toString()}</p>`;
+  response.send(responseHtml);
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    const errorMessage = {
+      error:
+        `${!body.name ? "name " : ""}` +
+        `${!body.name && !body.number ? "and " : ""}` +
+        `${!body.number ? "number " : ""}missing`,
+    };
+    return response.status(400).json(errorMessage);
+  } else {
+    if (persons.some((person) => body.name === person.name)) {
+      return response.status(400).json({ error: "name must be unique" });
+    }
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+    };
+    persons = persons.concat(person);
+    response.json(person);
+  }
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
