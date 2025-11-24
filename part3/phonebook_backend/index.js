b3e02b4efe2c38a278441e989f807509bfc5fb6a
Pedro Valdebenito <pedrovaldebto@gmail.com>
2025-11-24 13:37:56 -0300

    complete exercise 3.9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("body", (request, response) => {
   return request.body ? JSON.stringify(request.body) : "";
 });
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
@@ -96,7 +100,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PERSONS_API_PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
