97c63850777e537aadce64e029f9906725cdecd9
Pedro Valdebenito <pedrovaldebto@gmail.com>
2025-11-30 16:56:17 -0300

    fix rendering of phonebook front after delete
    Fixed the re rendering of phonebook's frontend after deleting an entry.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -95,9 +95,10 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
+  const personToDelete = persons.find((person) => person.id == id);
   persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
+  response.json(personToDelete).status(204);
 });
 
 const PORT = process.env.PERSONS_API_PORT || 3001;
